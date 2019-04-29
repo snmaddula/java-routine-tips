a58ab6b560433186a86b407cfaf5c87d0cb2d34e
SN MADDULA <snmaddula@gmail.com>
2019-04-29 10:55:16 +0530

    Update SwapDateFormat.java

--- snmaddula/date/SwapDateFormat.java
+++ snmaddula/date/SwapDateFormat.java
@@ -1,1 +1,26 @@
+package snmaddula.date;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * 
+ * @author snmaddula
+ *
+ */
+public class SwapDateFormat {
+  public static void main(String[] args) throws Exception {
+    DateFormat slashDF = new SimpleDateFormat("dd/mm/yyyy");
+    DateFormat dotDF = new SimpleDateFormat("dd.mm.yyyy");
+    String input = "15/04/1991";
+    String output = dotDF.format(slashDF.parse(input));
+    System.out.printf("INPUT  : %s\nOUTPUT : %s", input, output);
+  }
+}
+
+/********************
+OUTPUT
+------
+INPUT  : 15/04/1991
+OUTPUT : 15.04.1991
+*********************/
