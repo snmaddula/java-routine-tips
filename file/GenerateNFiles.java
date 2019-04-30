851ee14ddecc31e68a1bafe937e78e50339062aa
SN MADDULA <snmaddula@gmail.com>
2019-04-30 11:21:56 +0530

    Update GenerateNFiles.java

--- file/GenerateNFiles.java
+++ file/GenerateNFiles.java
@@ -1,1 +1,29 @@
+package snmaddula.file;
 
+import static java.nio.file.Files.write;
+import static java.nio.file.Paths.get;
+import static java.util.stream.IntStream.rangeClosed;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ * @author snmaddula
+ *
+ */
+public class GenerateNFiles {
+  
+  public static void main(String[] args) {
+    String content = "Hello Bhaaru!";
+    DateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+    rangeClosed(1, 10).forEach(i -> {
+			try {
+				write(get("FILE_" + df.format(new Date())), content.getBytes());
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		});
+  }
+}
