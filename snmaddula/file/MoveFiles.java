3a0984444d844ac52cc5f4d7581a8375ee0e965c
SN MADDULA <snmaddula@gmail.com>
2019-04-30 12:34:00 +0530

    Create MoveFiles.java

---
+++ snmaddula/file/MoveFiles.java
@@ -0,0 +1,6 @@
+package snmaddula.file;
+
+import static java.nio.file.Files.createDirectories;
+import static java.nio.file.Files.move;
+import static java.nio.file.Paths.get;
+import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
