8e88f5812f7032f98c58262c1d40602b50e494ba
SN MADDULA <snmaddula@gmail.com>
2019-04-30 12:34:46 +0530

    Update MoveFiles.java

--- snmaddula/file/MoveFiles.java
+++ snmaddula/file/MoveFiles.java
@@ -4,3 +4,21 @@ import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.move;
 import static java.nio.file.Paths.get;
 import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
+
+/**
+ * 
+ * @author snmaddula
+ *
+ */
+public class MoveFiles {
+  
+  public static void main(String[] args) throws Exception {
+    String fileName = "hello.txt";
+    String sourcePath = "C:/Users/USERNAME/Desktop/";
+    String targetPath = "C:/Users/USERNAME/Desktop/my_files/";
+    createDirectories(get(targetPath));
+    move(get(sourcePath), get(targetPath + fileName), ATOMIC_MOVE);
+  }
+}
+    
+    
